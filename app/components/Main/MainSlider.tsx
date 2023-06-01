03dc6194a0c7205348065effee2f9090f855a86f
blackweeed <blackweeeed@gmail.com>
2023-06-01 22:43:24 +0200

    working on home page layout

--- app/components/Main/MainSlider.tsx
+++ app/components/Main/MainSlider.tsx
@@ -28,6 +28,44 @@ const MainSlider = () => {
           w miejscu, gdzie natura odsłania swoje najpiękniejsze oblicze.
           Zapraszamy do Słonecznych Chatek!
         </p>
+        <div className="flex mt-8 font-beautyFont ">
+          <div className="flex items-center bg-black/50 gap-4 pr-6 cursor-pointer hover:bg-primary hover:text-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 ml-4"
+            >
+              <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+              <path
+                fillRule="evenodd"
+                d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="text-2xl">01</span>
+            <span className="pt-1">CZE</span>
+          </div>
+          <div className="flex items-center bg-black/50 gap-4 pr-4 hover:bg-primary hover:text-black cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 ml-4"
+            >
+              <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+              <path
+                fillRule="evenodd"
+                d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="text-2xl">02</span>
+            <span className="pt-1">CZE</span>
+          </div>
+
+          <button className="_button">REZERWUJ</button>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -63,8 +63,101 @@ export default function Home() {
         </div>
         <button className="button2 mt-16 self-center">ZOBACZ WIĘCEJ</button>
       </section>
-      <section className="px-28 h-[100vh] flex flex-col">
+      <section className="px-28 h-[100vh] flex flex-col relative overflow-x-hidden">
+        <div className="absolute -right-[33rem] top-[50%] -translate-y-[40%] ">
+          <img className="w-[78%] object-cover" src="/gora.png" alt="" />
+        </div>
         <Heading title="Okolica" />
+        <div className="flex items-center">
+          <div className="basis-[50%]">
+            <img
+              src="https://thumbs.hotelsystems.pl/slonecznechatki/_thumb/747x673/banery/Atrakcje/snowy-peaks-g42ed2ba4e_1920_copy_1.jpg"
+              alt=""
+              className="w-[80%] h-[95%] object-cover"
+            />
+          </div>
+          <div className="basis-[24%] flex flex-col gap-8">
+            <h4 className="font-semibold">
+              Noclegi z widokiem na Tatry w spokojnej okolicy
+            </h4>
+            <p className="z-10">
+              Marzy Wam się wypoczynek w górach, ale chcecie uniknąć tłumów i
+              zgiełku największych tatrzańskich kurortów? Domki w Knurowie to
+              strzał w dziesiątkę. Zielona, spokojna okolica i spektakularna
+              panorama na Tatry, Gorce i Zalew Czorsztyński to klucz do
+              prawdziwego odpoczynku.
+            </p>
+            <button className="_button2 mt-4">WIĘCEJ</button>
+          </div>
+        </div>
+      </section>
+      <section className="h-[150vh] px-28 mb-20">
+        <Heading title="Domki" />
+        <div className="mb-4 w-full h-[80vh]">
+          <img
+            src="https://sloneczne-chatki.pl/_thumb/1920x1080x80/img/slonecznechatki/rooms_types/035_knurow-domek.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex gap-4">
+          <div className="_shadow basis-[60%] flex justify-around items-center ">
+            <div className="">
+              <div className="font-beautyFont text-2xl mb-5">Domek 4</div>
+              <div>
+                <span className="text-primary">od </span>500 zł{" "}
+                <span className="text-primary">/ noc</span>
+              </div>
+            </div>
+            <div>
+              <div className="flex gap-6 mb-6">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-primary"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+                  />
+                </svg>
+                <span>90 m2</span>
+              </div>
+              <div className="flex gap-6">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6 text-primary"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.25 6.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM15.75 9.75a3 3 0 116 0 3 3 0 01-6 0zM2.25 9.75a3 3 0 116 0 3 3 0 01-6 0zM6.31 15.117A6.745 6.745 0 0112 12a6.745 6.745 0 016.709 7.498.75.75 0 01-.372.568A12.696 12.696 0 0112 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 01-.372-.568 6.787 6.787 0 011.019-4.38z"
+                    clipRule="evenodd"
+                  />
+                  <path d="M5.082 14.254a8.287 8.287 0 00-1.308 5.135 9.687 9.687 0 01-1.764-.44l-.115-.04a.563.563 0 01-.373-.487l-.01-.121a3.75 3.75 0 013.57-4.047zM20.226 19.389a8.287 8.287 0 00-1.308-5.135 3.75 3.75 0 013.57 4.047l-.01.121a.563.563 0 01-.373.486l-.115.04c-.567.2-1.156.349-1.764.441z" />
+                </svg>
+                <span>max 6 os.</span>
+              </div>
+            </div>
+            <div className="border-l-2 pl-10 py-4 border-primary grid grid-cols-3 gap-3">
+              <div className="w-8 h-8 border border-primary"></div>
+              <div className="w-8 h-8 border border-primary"></div>
+              <div className="w-8 h-8 border border-primary"></div>
+              <div className="w-8 h-8 border border-primary"></div>
+              <div className="w-8 h-8 border border-primary"></div>
+              <div className="w-8 h-8 border border-primary"></div>
+            </div>
+          </div>
+          <div className="_shadow  basis-[40%] w-full h-[10rem] flex justify-center items-center gap-[10%]">
+            <button className="_button2">SZCZEGÓŁY</button>
+            <button className="_button">REZERWUJ</button>
+          </div>
+        </div>
       </section>
     </main>
   );
